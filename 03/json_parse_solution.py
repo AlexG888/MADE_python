63740d9007f1ec57c0af0e2bb2d0de407a0cd776
Alexander Gerasimov <58679315+AlexG888@users.noreply.github.com>
2022-10-10 14:05:18 +0500

    Update json_parse_solution.py

--- 03/json_parse_solution.py
+++ 03/json_parse_solution.py
@@ -1,8 +1,10 @@
 import json
 
 
-def keyword_callback(v):
-    print(f"Найдено ключевое имя {v}")
+def keyword_callback():
+    global cnt
+    cnt += 1
+    return cnt
 
 
 def parse_json(keyword_callback, json_str: str, required_fields=None, keywords=None):
@@ -12,34 +14,44 @@ def parse_json(keyword_callback, json_str: str, required_fields=None, keywords=N
             if key in required_fields:
                 for v in value.split():
                     if v in keywords:
-                        keyword_callback(v)
+                        keyword_callback()
     else:
         print("Недостаточно данных!")
 
 
+cnt = 0
 json_str = '{"key1": "Word1 word2", "key2": "word2 word3"}'
 required_fields = ["key1"]
 keywords = ["word2"]
 assert parse_json(keyword_callback, json_str, required_fields, keywords) is None
+assert keyword_callback() == 1 + 1
 
+cnt = 0
 json_str = '{"key1": "Word1 word2", "key2": "word2 word3"}'
 required_fields = []
 keywords = ["word2"]
 assert parse_json(keyword_callback, json_str, required_fields, keywords) is None
+assert keyword_callback() == 0 + 1
 
+cnt = 0
 json_str = '{"key1": "Word1 word2", "key2": "word2 word3"}'
 required_fields = ["key1"]
 keywords = []
 assert parse_json(keyword_callback, json_str, required_fields, keywords) is None
+assert keyword_callback() == 0 + 1
 
+cnt = 0
 json_str = ""
 required_fields = ["key1"]
 keywords = ["word2"]
 assert parse_json(keyword_callback, json_str, required_fields, keywords) is None
+assert keyword_callback() == 0 + 1
 
+cnt = 0
 json_str = '{"key1": "Word1 word2", "key2": "word2 word3"}'
 required_fields = ["key1"]
 keywords = ["word2", "Word1"]
 assert parse_json(keyword_callback, json_str, required_fields, keywords) is None
+assert keyword_callback() == 2 + 1
 
 print("OK")
