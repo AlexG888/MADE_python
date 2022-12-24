2b2ebcb008372b4069fdf9f3feb05197e68f9845
Alexander Gerasimov <58679315+AlexG888@users.noreply.github.com>
2022-12-24 18:29:54 +0300

    Update client.py

--- advance_02/client.py
+++ advance_02/client.py
@@ -1,33 +1,56 @@
 from sys import argv
-import threading
+from threading import Thread
 import socket
-import json
+from queue import Queue
 
 
-def reader(url, semaphore):
-    with semaphore:
-        sock.send(url.encode())
-        data = sock.recv(1024)
-        print(json.loads(data.decode()))
+def connection(addr1, addr2):
+    sock1 = socket.socket()
+    sock2 = socket.socket()
+    sock1.connect(("", addr1))
+    sock2.connect(("", addr2))
+    return sock1, sock2
 
 
-def batch_reader(filename, semaphore):
-    with open(filename, encoding="utf8") as file:
-        for url in file.readlines():
-            reader(url, semaphore)
+def reader_url(queue, sock1, sock2):
+    while not queue.empty():
+        url = queue.get()
+        sock1.sendall(url.encode("utf-8"))
+        data = sock2.recv(4096).decode("utf-8")
+        print(data)
+
+
+def urls_list_maker(filename):
+    with open(filename, "r", encoding="utf8") as file:
+        urls_list = file.readlines()
+    return urls_list
+
+
+def client(thread_num, urls, addr=7500):
+    sock1, sock2 = connection(addr, addr + 100)
+    queue = Queue()
+    for url in urls:
+        if url[-1] != "\n":
+            queue.put(url + "\n")
+        else:
+            queue.put(url)
+    queue.put("-")
+    threads = [
+        Thread(target=reader_url, args=(queue, sock1, sock2)) for _ in range(thread_num)
+    ]
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    sock1.close()
+    sock2.close()
 
 
 if __name__ == "__main__":
-    sock = socket.socket()
-    sock.connect(("localhost", 7500))
-    m, filename = int(argv[1]), argv[2]
-    sem = threading.Semaphore(m)
-    threads = [
-        threading.Thread(
-            target=batch_reader,
-            args=(filename, sem),
-        )
-        for _ in range(m)
-    ]
-    for th in threads:
-        th.start()
+    thread_number = int(argv[1])
+    name = argv[2]
+    urls_list = urls_list_maker(name)
+    client(thread_number, urls_list)
